e6e8315255affd5ade5273028c4b792ad3cda9fc
prathibha2525 <pratibhahchavda07@gmail.com>
2023-01-18 14:33:06 +0530

    cors

--- src/main.py
+++ src/main.py
@@ -9,11 +9,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app =FastAPI() 
 
 origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:3000",
-    "http://localhost:3000/*",
+    "*"
 ]
 
 app.add_middleware(
