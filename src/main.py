2ad81eec5e2fae15a2385f16bbaa3d346e1bbb77
prathibha2525 <pratibhahchavda07@gmail.com>
2023-01-18 15:04:40 +0530

    addurl

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app =FastAPI() 
 
 origins = [
-    "*"
+    "*",
 ]
 
 app.add_middleware(
--- src/urls/v1/user_login1_url.py
+++ src/urls/v1/user_login1_url.py
@@ -11,31 +11,38 @@ from src.services.user_login1.serializer import UsersSerializer, QuerySerializer
 
 router = APIRouter(prefix="/fa_marketshare")
 
+@router.post("/login")
 @router.post("/login/")
 async def login(request:UsersSerializer,Authorize:AuthJWT=Depends(),db: Session = Depends(get_db)):
     return await Users_Module.login(request,Authorize,db)
 
+@router.post("/q")
 @router.post("/q/")
 async def query(request:QuerySerializer,db: Session = Depends(get_db),Authorize:AuthJWT=Depends()):
     return await Users_Module.query(request,db,Authorize)
 
+@router.post("/saveq")
 @router.post("/saveq/")
 async def saveq(request:SaveSerializer,db: Session = Depends(get_db)):
     return await Users_Module.saveq(request,db)
 
+@router.post("/updateq")
 @router.post("/updateq/")
 async def updateq(request:UpdateSerializer,db: Session = Depends(get_db)):
     return await Users_Module.updateq(request,db)
 
 
+@router.post("/listq")
 @router.post("/listq/")
 async def listq(request:LoadSerializer,db: Session = Depends(get_db)):
     return await Users_Module.listq(request,db)
 
+@router.post("/deleteq")
 @router.post("/deleteq/")
 async def deleteq(request:DeleteSerializer,db: Session = Depends(get_db)):
     return await Users_Module.deleteq(request,db)
 
+@router.post("/logoutq")
 @router.post("/logoutq/")
 async def logoutq(request:LogoutSerializer,db: Session = Depends(get_db)):
     return await Users_Module.logoutq(request,db)
@@ -43,19 +50,22 @@ async def logoutq(request:LogoutSerializer,db: Session = Depends(get_db)):
 #####
 
 
-
+@router.post("/csv")
 @router.post("/csv/")
 async def csv(request:CsvSerializer):
     return await Users_Module.csv(request)
 
+@router.post("/txt")
 @router.post("/txt/")
 async def txt(request:CsvSerializer):
     return await Users_Module.txt(request)
 
+@router.post("/xls")
 @router.post("/xls/")
 async def xls(request:CsvSerializer):
     return await Users_Module.xls(request)
 
+@router.post("/pdf")
 @router.post("/pdf/")
 async def pdf(request:CsvSerializer):
     return await Users_Module.pdf(request)
